e0255c7411e7919ea93b7253fb848b120f0d76ad
kalash <kalashvasaniya@gmail.com>
2023-08-17 17:52:30 +0530

    17/08 - Update Details api

--- pages/api/details/index.js
+++ pages/api/details/index.js
@@ -1,47 +1,49 @@
-import Details from '@/models/Details';
 import User from '@/models/User';
+import Details from '@/models/Details';
 
 export default async function handler(req, res) {
     try {
-        console.log("hellooooo")
         const user = await User.findOne({
             verify: true
         });
-        console.log(user)
 
-        // If verified user is not found, return unauthorized
         if (!user) {
             return res.status(401).json({ message: "Unauthorized" });
         }
-        // Check if the request method is POST
+
         if (req.method === "POST") {
             try {
-                // Create a new details record with the same user ID as token.userId and user._id
-                const details = await Details.create({
-                    _id: user._id,
-                    relation: req.body.relation,
-                    year: req.body.year,
-                    LoveTo: req.body.LoveTo,
-                    gender: req.body.gender
-                });
-                console.log(details)
-                // Return success response with created details record
+                let details = await Details.findOne({ user: user._id });
+
+                if (details) {
+                    // If details exist, update the fields instead of creating new
+                    details.relation = req.body.relation;
+                    details.year = req.body.year;
+                    details.LoveTo = req.body.LoveTo;
+                    details.gender = req.body.gender;
+                    await details.save();
+                } else {
+                    // If details don't exist, create new
+                    details = await Details.create({
+                        user: user._id,
+                        relation: req.body.relation,
+                        year: req.body.year,
+                        LoveTo: req.body.LoveTo,
+                        gender: req.body.gender
+                    });
+                }
+
                 user.details = true;
                 await user.save();
+
                 return res.status(201).json({ success: true, details });
             } catch (err) {
-                // Return error response if details creation fails
                 return res.status(400).json({ message: err.message });
             }
-        }
-        else {
-            // Return method not allowed if request method is not POST
+        } else {
             return res.status(405).json({ message: "Method Not Allowed" });
         }
-
     } catch (error) {
-        // Return error response for any other unexpected errors
         return res.status(500).json({ message: "Internal Server Error" });
     }
-
 }
