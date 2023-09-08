9fbacf8946c0c1dcfac9a77f4c426252859105c2
kalash <kalashvasaniya@gmail.com>
2023-09-08 16:47:01 +0530

    8/09 - Like Toggle Done update remain

--- app/Feed/page.js
+++ app/Feed/page.js
@@ -256,12 +256,28 @@ const Feed = () => {
                                                                     <div className="flex flex-row space-x-4">
 
                                                                         {/* Here  */}
-                                                                        <button key={index} onClick={() => {
-                                                                            handleButtonClick(userPost._id)
-                                                                        }} className="pl-4 flex flex-row justify-center items-center space-x-2">
+                                                                        <button className={`pl-4 flex flex-row justify-center items-center space-x-2 ${post.like > 0 ? 'liked' : ''}`}
+                                                                            key={index}
+                                                                            onClick={async (e) => {
+                                                                                e.preventDefault();
 
+                                                                                // Toggle the 'liked' class on the button element
+                                                                                const likeButton = e.currentTarget;
+
+                                                                                // Update the like count based on the presence of the 'liked' class
+                                                                                if (likeButton.classList.contains('liked')) {
+                                                                                    post.like += 1;
+                                                                                } else {
+                                                                                    post.like -= 1;
+                                                                                }
+
+                                                                                likeButton.classList.toggle('liked');
+                                                                                // Perform your handleButtonClick(post._id) action here
+                                                                                handleButtonClick(post._id);
+                                                                            }}
+                                                                        >
                                                                             <div className="">
-                                                                                {!isIcon[userPost._id] ? (
+                                                                                {!isIcon[post._id] ? (
                                                                                     <svg
                                                                                         viewBox="0 0 1024 1024"
                                                                                         fill="currentColor"
@@ -277,15 +293,21 @@ const Feed = () => {
                                                                                         fill="currentColor"
                                                                                         height="1em"
                                                                                         width="1em"
-                                                                                        className='hover:scale-105 w-7 h-7 flex justify-center items-center mb-1'
-                                                                                    >
+                                                                                        className='hover:scale-105 w-7 h-7 flex justify-center items-center mb-1'>
                                                                                         <path d="M923 283.6a260.04 260.04 0 00-56.9-82.8 264.4 264.4 0 00-84-55.5A265.34 265.34 0 00679.7 125c-49.3 0-97.4 13.5-139.2 39-10 6.1-19.5 12.8-28.5 20.1-9-7.3-18.5-14-28.5-20.1-41.8-25.5-89.9-39-139.2-39-35.5 0-69.9 6.8-102.4 20.3-31.4 13-59.7 31.7-84 55.5a258.44 258.44 0 00-56.9 82.8c-13.9 32.3-21 66.6-21 101.9 0 33.3 6.8 68 20.3 103.3 11.3 29.5 27.5 60.1 48.2 91 32.8 48.9 77.9 99.9 133.9 151.6 92.8 85.7 184.7 144.9 188.6 147.3l23.7 15.2c10.5 6.7 24 6.7 34.5 0l23.7-15.2c3.9-2.5 95.7-61.6 188.6-147.3 56-51.7 101.1-102.7 133.9-151.6 20.7-30.9 37-61.5 48.2-91 13.5-35.3 20.3-70 20.3-103.3.1-35.3-7-69.6-20.9-101.9z" />
-                                                                                    </svg>)}
+
+                                                                                    </svg>
+                                                                                )}
                                                                             </div>
-                                                                            <button key={index} className='text-xs flex justify-center items-center hover:underline' href={''}>
+                                                                            <button
+                                                                                key={index}
+                                                                                className='text-xs flex justify-center items-center hover:underline'
+                                                                                href={''}
+                                                                            >
                                                                                 {post.like} Like
                                                                             </button>
                                                                         </button>
+
 
 
                                                                         <button key={index} className="px-4 flex flex-row justify-center items-center space-x-2">
