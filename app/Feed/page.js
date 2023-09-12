82e67b2cd576fb5d34788799dbd7d4b65eddffb9
kalash <kalashvasaniya@gmail.com>
2023-09-12 17:28:54 +0530

    12/09 - Remove EXT

--- app/Feed/page.js
+++ app/Feed/page.js
@@ -18,7 +18,7 @@ const Feed = () => {
     const [showTooltip2, setShowTooltip2] = useState(false);
     const [isIcon, setIsIcon] = useState(false);
     const [isCopied, setIsCopied] = useState(false);
-    const [showPopup, setShowPopup] = useState(false);
+    const [like, setLike] = useState('')
 
     useEffect(() => {
         setTimeout(() => {
@@ -78,17 +78,6 @@ const Feed = () => {
         const date = new Date(inputDate);
         return date.toLocaleDateString('en-IN', options);
     }
-
-    const handleButtonClick2 = (userId1) => {
-        setShowPopup((prevVisibility) => ({
-            ...prevVisibility,
-            [userId1]: !prevVisibility[userId1] || false,
-        }));
-    }
-
-    const closePopup = () => {
-        setShowPopup(false);
-    };
 
     const { highlightHashTags } = useTPU();
 
@@ -313,49 +302,9 @@ const Feed = () => {
                                                                             <div className="relative">
                                                                                 <button onClick={(e) => {
                                                                                     e.preventDefault()
-                                                                                    handleButtonClick2(post._id)
                                                                                 }} className='text-xs flex justify-center items-center hover:underline'>
                                                                                     {post.like} Like
                                                                                 </button>
-
-                                                                                {showPopup[post._id] && (
-                                                                                    <div className="absolute bg-neutral-900 p-4 z-50 flex flex-col rounded-2xl">
-                                                                                        <div className="flex justify-center items-center space-x-40 text-center">
-                                                                                            <div className="flex justify-center items-center text-center flex-row space-x-2">
-                                                                                                <div className="text-base font-semibold font-mono text-sky-400">Like</div>
-                                                                                                <span className="text-base font-semibold font-mono text-sky-400"> by</span>
-                                                                                            </div>
-                                                                                            {/* Close Button  */}
-                                                                                            <button className='' onClick={closePopup}>
-                                                                                                <div
-                                                                                                    type="button"
-                                                                                                    className="text-gray-400 bg-transparent hover:bg-sky-500 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5"
-                                                                                                >
-                                                                                                    <svg
-                                                                                                        aria-hidden="true"
-                                                                                                        className="w-4 h-4"
-                                                                                                        fill="currentColor"
-                                                                                                        viewBox="0 0 20 20"
-                                                                                                        xmlns="http://www.w3.org/2000/svg"
-                                                                                                    >
-                                                                                                        <path
-                                                                                                            fillRule="evenodd"
-                                                                                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                                                                                            clipRule="evenodd"
-                                                                                                        ></path>
-                                                                                                    </svg>
-                                                                                                    <span className="sr-only">Close menu</span>
-                                                                                                </div>
-                                                                                            </button>
-                                                                                        </div>
-
-                                                                                        <hr />
-
-                                                                                        <div className="flex flex-col">
-
-                                                                                        </div>
-                                                                                    </div>
-                                                                                )}
                                                                             </div>
                                                                         </button>
 
