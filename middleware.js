ec706200b2bfa9e3e0ebe9618b1b8837540052a9
kalash <kalashvasaniya@gmail.com>
2024-07-31 19:14:30 +0530

    init

--- middleware.js
+++ middleware.js
@@ -1,9 +1,8 @@
 import { NextResponse } from 'next/server';
-import { MongoClient } from 'mongodb';
+import mongoose from 'mongoose';
 
 // Not Required
 // This function can be marked `async` if using `await` inside
-
 export function middleware(request) {
     return NextResponse.redirect(new URL('/', request.url));
 }
@@ -12,18 +11,45 @@ export const config = {
     matcher: '/afrojack',
 };
 
-const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI);
+const db = mongoose.connection;
 
-async function connectToDatabase() {
-    try {
-        await client.connect();
-        console.log('Connected to MongoDB & The Goal is to Reach The PU');
-        return client.db(); // Return the database instance
-    } catch (error) {
-        console.error('MongoDB connection error:', error);
-    }
-}
+db.on('error', (error) => {
+    console.error('MongoDB connection error:', error);
+});
 
-const db = connectToDatabase();
+db.once('open', () => {
+    console.log('Connected to MongoDB & The Goal is to Reach The PU');
+});
 
 export default db;
+
+// import { NextResponse } from 'next/server';
+// import { MongoClient } from 'mongodb';
+
+// // Not Required
+// // This function can be marked `async` if using `await` inside
+
+// export function middleware(request) {
+//     return NextResponse.redirect(new URL('/', request.url));
+// }
+// // See "Matching Paths" below to learn more
+// export const config = {
+//     matcher: '/afrojack',
+// };
+
+// const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// async function connectToDatabase() {
+//     try {
+//         await client.connect();
+//         console.log('Connected to MongoDB & The Goal is to Reach The PU');
+//         return client.db(); // Return the database instance
+//     } catch (error) {
+//         console.error('MongoDB connection error:', error);
+//     }
+// }
+
+// const db = connectToDatabase();
+
+// export default db;
