91dfb525f75a55da9a8d156af0aa31076a1af369
kalash <kalashvasaniya@gmail.com>
2024-08-01 16:39:04 +0530

    Revert "Merge branch 'main' of https://github.com/kalashvasaniya/the-pair-up"
    This reverts commit b2834989afd9bb5d343bced05059377d5f244157, reversing
    changes made to f0c7d6a5aecd880f599b1c8413ebddca0752334f.

--- app/user/login/page.js
+++ app/user/login/page.js
@@ -18,7 +18,7 @@ const Login = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        const res = await fetch(`api/login`, {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
