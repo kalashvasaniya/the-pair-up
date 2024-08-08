c9ef61d0fe23d2a4b0b8ac775faf7302a75e0b73
kalash <kalashvasaniya@gmail.com>
2024-08-08 10:22:39 +0530

    init

--- pages/api/searchUser/index.js
+++ pages/api/searchUser/index.js
@@ -1,11 +1,12 @@
 import User from '@/models/User'; // Your User model
 import Details from '@/models/Details'; // Your Details model
-import Post from '@/models/Post';
-import connect from '@/lib/db';
+import Post from '@/models/Post'; // Your Post model
+import connect from '@/lib/db'; // Your database connection function
 
 export default async function handler(req, res) {
     await connect(); // Ensure a database connection
-    if (req.method !== 'GET') {
+
+    if (req.method === 'GET') {
         try {
             const { slug } = req.query;
 
@@ -19,12 +20,13 @@ export default async function handler(req, res) {
                 user: { $in: users.map(user => user._id) }
             });
 
+            // Query the database for posts associated with the found users
             const posts = await Post.find({
                 user: { $in: users.map(user => user._id) }
-            })
+            });
 
             // Return the matching users and their details
-            return res.status(200).json({ "success": true, users, details, posts });
+            return res.status(200).json({ success: true, users, details, posts });
         } catch (error) {
             console.error(error);
             return res.status(500).json({ error: 'Internal server error' });
