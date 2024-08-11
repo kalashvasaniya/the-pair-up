f97cbdef9a89c646b87c847c9f4668044cf1c8ac
kalash <kalashvasaniya@gmail.com>
2024-08-11 17:22:49 +0530

    SMTP is not supporting 3rd party app like Nodemailer

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,24 +1,28 @@
 const nodemailer = require("nodemailer");
 
 async function sendEmail(req, res) {
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.USER,
-            pass: process.env.PASS,
-        },
-    });
-    const mailOptions = {
-        from: process.env.USER,
-        to: req.email,
-        subject: req.subject,
-        text: req.text,
-    };
     try {
-        await transporter.sendMail(mailOptions);
-        console.log("Email sent successfully");
-    } catch (err) {
-        console.log("Error sending email", err);
+        const { email, subject, text } = req;
+        const transporter = nodemailer.createTransport({
+            host: process.env.HOST,
+            service: process.env.SERVICE,
+            port: Number(process.env.EMAIL_PORT),
+            secure: Boolean(process.env.SECURE),
+            auth: {
+                user: process.env.USER,
+                pass: process.env.PASS
+            }
+        });
+
+        await transporter.sendMail({
+            from: process.env.USER,
+            to: email,
+            subject: subject,
+            text: text
+        });
+        console.log("Email sent successfully")
+    } catch (error) {
+        console.error("Error sending email:", error);
     }
 }
 
