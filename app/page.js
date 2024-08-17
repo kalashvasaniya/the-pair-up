49a628811c92dc9e1e117c77fe5be3894d2486ee
kalash <kalashvasaniya@gmail.com>
2024-08-17 20:31:52 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -74,6 +74,8 @@ export default function Main() {
           <Footer />
         </>
       )}
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3582323814152958"
+        crossorigin="anonymous"></script>
     </>
   )
 }
