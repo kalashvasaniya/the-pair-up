8c74552e937e3ba8885ee13a1bfa7205c340b0bb
kalash <kalashvasaniya@gmail.com>
2024-08-17 20:31:58 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -130,8 +130,6 @@ export default function RootLayout({ children }) {
           <Analytics />
           <SpeedInsights />
         </div>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3582323814152958"
-          crossorigin="anonymous"></script>
       </body>
     </html >
   )
